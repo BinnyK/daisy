bb160b2fbc5c5f9453541603508341d1dcfd3914
BinnyK <BinnyK@users.noreply.github.com>
2017-06-01 20:38:57 +1000

    Add flowers to flowers.js

--- src/flowers.js
+++ src/flowers.js
@@ -45,11 +45,12 @@ const flowers = [
     "name": "Bud Wax",
     "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
     "availability": [
-      "oct",
-      "nov",
-      "dec",
-      "jan",
-      "feb",
+      "apr",
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sep"
     ],
     url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
   },
@@ -67,13 +68,403 @@ const flowers = [
   },
   {
     "id": 7,
-    "name": "TESTING",
+    "name": "Carnation",
     "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
     "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 8,
+    "name": "Celosia",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 9,
+    "name": "Daffodil",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "jun",
+      "jul",
+      "aug"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 10,
+    "name": "Dahlia",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "dec",
+      "jan",
+      "feb",
       "mar"
     ],
     url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
   },
+  {
+    "id": 11,
+    "name": "Daisy",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 12,
+    "name": "David Austin",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "nov",
+      "dec",
+      "jan"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 13,
+    "name": "Delphinium",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 14,
+    "name": "Erlicheer Jonquil",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "jun",
+      "jul",
+      "aug",
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 15,
+    "name": "Frangipani",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "sep",
+      "oct",
+      "nov",
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 16,
+    "name": "Freesia",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "aug",
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 17,
+    "name": "Gardenia",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 18,
+    "name": "Freesia",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "aug",
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 19,
+    "name": "Geraldton Waxflower",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "jun",
+      "jul",
+      "aug",
+      "sep",
+      "nov"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 20,
+    "name": "Gerbera",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 21,
+    "name": "Babies Breath (Gypsophlia)",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "aug",
+      "sep",
+      "oct",
+      "nov",
+      "dec",
+      "jan",
+      "feb",
+      "mar",
+      "apr"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 22,
+    "name": "Hyacinth",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "jun",
+      "jul",
+      "aug",
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 23,
+    "name": "Hydrangea",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 24,
+    "name": "Iris",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sep",
+      "oct"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 25,
+    "name": "Jasmine",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 26,
+    "name": "Lavender",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "oct",
+      "nov",
+      "jan"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 27,
+    "name": "Lily of the Valley",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "oct"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 28,
+    "name": "Lisianthus",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "dec",
+      "jan",
+      "mar"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 29,
+    "name": "Oriental Lilium",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 30,
+    "name": "Peony",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "oct",
+      "nov"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 31,
+    "name": "Poppy",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "apr",
+      "may",
+      "jun",
+      "jul",
+      "aug"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 32,
+    "name": "Rose",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 33,
+    "name": "Singapore Orchid",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 34,
+    "name": "Snapdragon",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 35,
+    "name": "Spray Carnation",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "year"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 36,
+    "name": "Statice",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "sep",
+      "oct",
+      "nov",
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 37,
+    "name": "Stock",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sep",
+      "oct"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 38,
+    "name": "Sunflower",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "sep",
+      "oct",
+      "nov",
+      "dec",
+      "jan",
+      "feb"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 39,
+    "name": "Sweet Pea",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sep"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 40,
+    "name": "Tulip",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "apr",
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sep",
+      "oct"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  },
+  {
+    "id": 41,
+    "name": "Tulip",
+    "desc": "Calla Lilies are extremely popular with style gurus for their sculptural elegant shape.The most popular colour shades are the vibrant canary yellow and the tangerine orange tones.",
+    "availability": [
+      "mar",
+      "apr",
+      "may",
+      "jun",
+      "jul",
+      "aug"
+    ],
+    url: "http://webneel.com/wallpaper/sites/default/files/images/01-2014/20-flower-wallpaper.preview.jpg"
+  }
+
 ]
 
 export default flowers;
